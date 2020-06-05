54aadb9a19f074d3f51c73e4b9150315941103e0
Mateus Ferrando <mateus.ferrando@paytrack.com.br>
2020-06-05 17:24:14 -0300

    Implementado o carregar mais no gif

--- buscador_de_gifs/lib/ui/HomePage.dart
+++ buscador_de_gifs/lib/ui/HomePage.dart
@@ -21,7 +21,7 @@ class _HomePageState extends State<HomePage> {
           "https://api.giphy.com/v1/gifs/trending?api_key=vNf0TFD6xpU5e5ErpdOYti9ohjvGHb8u&limit=10&rating=G");
     else
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=vNf0TFD6xpU5e5ErpdOYti9ohjvGHb8u&q=$_search&limit=10&offset=$_offset&rating=G&lang=en");
+          "https://api.giphy.com/v1/gifs/search?api_key=vNf0TFD6xpU5e5ErpdOYti9ohjvGHb8u&q=$_search&limit=9&offset=$_offset&rating=G&lang=en");
 
     return json.decode(response.body);
   }
@@ -55,6 +55,12 @@ class _HomePageState extends State<HomePage> {
                   border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                  _offset = 0;
+                });
+              },
             ),
           ),
           Expanded(
@@ -84,21 +90,53 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-}
 
-Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
-  return GridView.builder(
-      padding: EdgeInsets.all(10.0),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
-      itemCount: snapshot.data["data"].length,
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          child: Image.network(
-            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
-            height: 100.0,
-            fit: BoxFit.cover,
-          ),
-        );
-      });
+  int _getCount(List data) {
+    if (_search == null)
+      return data.length;
+    else
+      return data.length + 1;
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: _getCount(snapshot.data["data"]),
+        itemBuilder: (context, index) {
+          if (_search == null || index < snapshot.data["data"].length)
+            return GestureDetector(
+              child: Image.network(
+                snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+                height: 100.0,
+                fit: BoxFit.cover,
+              ),
+            );
+          else
+            return Container(
+              child: GestureDetector(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 70.0,
+                    ),
+                    Text(
+                      "Carregar mais..",
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),
+                    )
+                  ],
+                ),
+                onTap: () {
+                  setState(() {
+                    _offset += 9;
+                  });
+                },
+              ),
+            );
+        });
+  }
 }
