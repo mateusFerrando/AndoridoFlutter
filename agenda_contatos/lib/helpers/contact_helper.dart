e5b10e1dcd5aa771f291e189135ecaa52b7fdbbc
Mateus Ferrando <mateus.ferrando@paytrack.com.br>
2020-06-05 18:43:30 -0300

    Implementado a criação do banco e das tabelas que serão utilizadas

--- agenda_contatos/lib/helpers/contact_helper.dart
+++ agenda_contatos/lib/helpers/contact_helper.dart
@@ -1,12 +1,46 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "emailColumn";
 final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
-class ContactHelper {}
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal() {}
+
+  Database _db;
+
+  Future<Database> get db async {
+    if (_db != null)
+      return _db;
+    else
+      _db = await initDb();
+    return _db;
+  }
+
+  Future<Database> initDb() async {
+    final dataBasesPath = await getDatabasesPath();
+    final path = join(dataBasesPath, "contacts.db");
+
+    return await openDatabase(
+        path, version: 1, onCreate: (Database db, int newVersion) async {
+      await db.execute(""
+          " CREATE TABLE $contactTable("
+          " $idColumn INTEGER PRIMARY KEY,"
+          " $nameColumn TEXT,"
+          " $emailColumn TEXT,"
+          " $phoneColumn TEXT,"
+          " $imgColumn TEXT)");
+    })
+  }
+}
 
 class Contact {
   int id;
