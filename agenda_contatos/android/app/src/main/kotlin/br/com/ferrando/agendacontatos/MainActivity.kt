6e5b416327cfc59ebecb49e2b398f1c5e7b2548d
Mateus Ferrando <mateus.ferrando@paytrack.com.br>
2020-06-05 17:55:27 -0300

    Iniciado o projeto de agenda de contatos, Mapeado os dados a serem utilizados

---
+++ agenda_contatos/android/app/src/main/kotlin/br/com/ferrando/agendacontatos/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.ferrando.agendacontatos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ agenda_contatos/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '';
+
+void main() {
+  runApp(MaterialApp(
+    home: Container(),
+  ));
+}
