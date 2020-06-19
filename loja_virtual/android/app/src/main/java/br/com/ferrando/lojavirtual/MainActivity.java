a819772810d4a2884b8e0aed258ed6b83609ff94
Mateus Ferrando <mateus.ferrando@paytrack.com.br>
2020-06-19 17:55:32 -0300

    Loja virtual, configurado o firebase e criada a tela inicial

---
+++ loja_virtual/android/app/src/main/java/br/com/ferrando/lojavirtual/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.ferrando.lojavirtual;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- loja_virtual/android/app/src/main/kotlin/br/com/ferrando/lojavirtual/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package br.com.ferrando.lojavirtual
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
