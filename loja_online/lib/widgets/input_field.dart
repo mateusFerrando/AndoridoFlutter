963f80b11e86b31e32de0d48fe35968a8815d5c0
Mateus Ferrando <mateus.ferrando@paytrack.com.br>
2020-07-03 15:32:43 -0300

    Implementada o BLOC e o validator do login

--- loja_online/lib/widgets/input_field.dart
+++ loja_online/lib/widgets/input_field.dart
@@ -4,25 +4,34 @@ class InputField extends StatelessWidget {
   final IconData icon;
   final String hint;
   final bool obscure;
+  final Stream<String> stream;
+  final Function(String) onChanged;
 
-  InputField({this.icon, this.hint, this.obscure});
+  InputField({this.icon, this.hint, this.obscure, this.stream, this.onChanged});
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-          icon: Icon(
-            icon,
-            color: Colors.white,
-          ),
-          hintText: hint,
-          contentPadding:
-              EdgeInsets.only(left: 5, right: 30, top: 30, bottom: 30),
-          hintStyle: TextStyle(color: Colors.white),
-          focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.pinkAccent))),
-      style: TextStyle(color: Colors.white),
-      obscureText: obscure,
+    return StreamBuilder<String>(
+      stream: stream,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: onChanged,
+          decoration: InputDecoration(
+              icon: Icon(
+                icon,
+                color: Colors.white,
+              ),
+              hintText: hint,
+              contentPadding:
+                  EdgeInsets.only(left: 5, right: 30, top: 30, bottom: 30),
+              errorText: snapshot.hasError ? snapshot.error:null,
+              hintStyle: TextStyle(color: Colors.white),
+              focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.pinkAccent))),
+          style: TextStyle(color: Colors.white),
+          obscureText: obscure,
+        );
+      },
     );
   }
 }
